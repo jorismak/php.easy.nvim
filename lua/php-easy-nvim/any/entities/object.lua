87a2997fc278ff716e9d89a0c4a927321bc96d1c
Joris Mak <47725838+jorismak@users.noreply.github.com>
2024-06-13 21:23:17 +0200

    attempt at decoding composer.json

--- lua/php-easy-nvim/any/entities/object.lua
+++ lua/php-easy-nvim/any/entities/object.lua
@@ -1,91 +1,97 @@
-local Config = require('php-easy-nvim.any.config')
+local Config = require("php-easy-nvim.any.config")
 
 local M = {}
 
 function M.docBlock()
-    vim.fn.setreg('p', '/**\n * \n */\n')
-    vim.cmd([[
+	vim.fn.setreg("p", "/**\n * \n */\n")
+	vim.cmd([[
         normal "pPj
         startinsert!
     ]])
 end
 
 local function initObject(type)
-    -- prepare name and path
-    local file = vim.fn.expand('%:t:r')
-    local path = vim.fn.fnamemodify(vim.fn.expand('%:p:h'), ':~:.')
+	local json = require("json")
+	-- prepare name and path
+	local file = vim.fn.expand("%:t:r")
+	local path = vim.fn.fnamemodify(vim.fn.expand("%:p:h"), ":~:.")
 
-    -- find psr4 in composer.json
-    local psr4 = {}
-    local is_psr4 = false
-    local composerPath = vim.fn.getcwd() .. '/composer.json', 'r'
-    for line in io.lines(composerPath) do
-        if line == '        }' then
-            is_psr4 = false
-        end
+	-- find psr4 in composer.json
+	local psr4 = {}
+	local is_psr4 = false
+	local composerPath = vim.fn.getcwd() .. "/composer.json", "r"
+	local f = io.open(composerPath, "rb")
+	local composerContentString = f:read("*all")
+	f:close()
 
-        if is_psr4 then
-            local parts = vim.split(vim.trim(line):gsub('[" ,]+', ''), ':')
-            psr4[parts[1]:gsub('\\\\', '\\')] = parts[2]
-        end
+	local composerContent = json.decode(composerContentString)
+	vim.print(composerContentString)
+	for line in io.lines(composerPath) do
+		if line == "    }" then
+			is_psr4 = false
+		end
 
-        if line == '        "psr-4": {' then
-            is_psr4 = true
-        end
-    end
+		if is_psr4 then
+			local parts = vim.split(vim.trim(line):gsub('[" ,]+', ""), ":")
+			psr4[parts[1]:gsub("\\\\", "\\")] = parts[2]
+		end
 
-    -- fix by psr4
-    if vim.tbl_count(psr4) > 0 then
-        for value, key in pairs(psr4) do
-            path = string.gsub(path, key, value)
-        end
-    end
+		if line == '    "psr-4": {' then
+			is_psr4 = true
+		end
+	end
 
-    -- fix slashes
-    path = path:gsub('/', '\\')
+	-- fix by psr4
+	if vim.tbl_count(psr4) > 0 then
+		for value, key in pairs(psr4) do
+			path = string.gsub(path, key, value)
+		end
+	end
 
-    if vim.fn.search("^namespace ", "w") == 0 then
-        -- empty file
-        vim.cmd([[
+	-- fix slashes
+	path = path:gsub("/", "\\")
+
+	if vim.fn.search("^namespace ", "w") == 0 then
+		-- empty file
+		vim.cmd([[
             normal! i<?php
             normal! o
             normal! odeclare(strict_types=1);
             normal! o
             normal! onamespace ]] .. path .. [[;
             normal! o
-            normal! o]] .. type .. [[ ]] .. file
-        );
-        vim.cmd([[
+            normal! o]] .. type .. [[ ]] .. file)
+		vim.cmd([[
             normal! o{
             normal! o}
         ]])
-        vim.fn.search(type .. ' ' .. file, 'e')
-    else
-        -- fix exists data
-        vim.cmd('normal! Cnamespace ' .. path .. ';')
-        vim.fn.search(Config.regex.object, 'we')
-        vim.cmd('normal! lcw' .. file)
-    end
+		vim.fn.search(type .. " " .. file, "e")
+	else
+		-- fix exists data
+		vim.cmd("normal! Cnamespace " .. path .. ";")
+		vim.fn.search(Config.regex.object, "we")
+		vim.cmd("normal! lcw" .. file)
+	end
 end
 
 function M.initInterface()
-    initObject('interface')
+	initObject("interface")
 end
 
 function M.initClass()
-    initObject('final class')
+	initObject("class")
 end
 
 function M.initAbstractClass()
-    initObject('abstract class')
+	initObject("abstract class")
 end
 
 function M.initTrait()
-    initObject('trait')
+	initObject("trait")
 end
 
 function M.initEnum()
-    initObject('enum')
+	initObject("enum")
 end
 
 return M
